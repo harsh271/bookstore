b08cc9ad39df29947964ddc6632c2cc6eb047b11
srashti <harshsharma271@gmail.com>
2020-07-20 17:25:05 +0530

    enter your commit message here describing

---
+++ src/main/java/com/javainuse/db/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.javainuse.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javainuse.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category,Integer>{
+
+}
