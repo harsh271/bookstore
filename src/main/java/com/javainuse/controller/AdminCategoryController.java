40213ae85701be1fdb387e2b7ae73ae6e88ec72a
harsh <harsh@arishi.agency>
2020-07-22 13:43:30 +0530

    30.POST ADD CATEGORY

--- src/main/java/com/javainuse/controller/AdminCategoryController.java
+++ src/main/java/com/javainuse/controller/AdminCategoryController.java
@@ -2,14 +2,22 @@ package com.javainuse.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.javainuse.db.CategoryRepository;
 import com.javainuse.model.Category;
+import com.javainuse.model.Page;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -21,7 +29,43 @@ public class AdminCategoryController {
 		List<Category> categories =ceRepository.findAll();
 		model.addAttribute("categories",categories);
 		return "admin/categories/index";
+	}
+	@ModelAttribute("category")
+	public Category getCategory() {
+		return new Category();
+	}
+	
+	@GetMapping("/add")
+	public String add() {
+		return "admin/categories/add";
+	}
+	@PostMapping("/add")
+	public String add(@Valid Category category, BindingResult bindginresult, RedirectAttributes redirectattributes,
+			Model model) {
+		if (bindginresult.hasErrors()) {
+			return "admin/categories/add";
+		}
+		redirectattributes.addFlashAttribute("message", "Category added");
+		redirectattributes.addFlashAttribute("alertClass", "alert-success");
+		String slug = category.getName().toLowerCase().replace(" ", "-");
+		Category categoryExists=ceRepository.findByName(slug);
 		
+		if (categoryExists != null) {
+			redirectattributes.addFlashAttribute("message", "Category exists,choose another");
+			redirectattributes.addFlashAttribute("alertClass", "alert-danger");
+			redirectattributes.addFlashAttribute("categoryInfo", category);
+		} else {
+		category.setSlug(slug);
+		category.setSorting(100);
+			ceRepository.save(category);
+		}
+		return "redirect:/admin/categories/add";
+	}
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id,Model model) {
+		Category category=ceRepository.getOne(id);
+		model.addAttribute("category",category);
+		return "admin/pages/edit";
 	}
 
 }
--- src/main/java/com/javainuse/db/CategoryRepository.java
+++ src/main/java/com/javainuse/db/CategoryRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.javainuse.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category,Integer>{
+	
+	Category findByName(String name);
 
 }
